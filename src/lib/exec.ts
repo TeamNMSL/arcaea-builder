8d5520332b8f5b056fd36fff6fb9704f6f99008d
Menci <huanghaorui301@gmail.com>
2021-06-23 15:45:54 +0800

    Fix tools paths provided by project config are not used

--- src/lib/exec.ts
+++ src/lib/exec.ts
@@ -3,7 +3,7 @@ import child_process from "child_process";
 import { ProjectConfig } from "@/config";
 
 export async function exec(tool: keyof ProjectConfig["tools"], args: string[]) {
-  const argv = [tool, ...args];
+  const argv = [projectConfig.tools[tool], ...args];
   logger.info(`Execuating [${argv.map(s => JSON.stringify(s)).join(", ")}]`);
   child_process.execFileSync(argv[0], argv.slice(1), {
     shell: false,
